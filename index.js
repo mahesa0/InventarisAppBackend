52b66018bfa2383265582293b1307950b44adedc
Thorik <axe.army194@gmail.com>
2024-10-23 22:03:04 +0700

    finishing

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
-import multer from "multer";
-import path from "path";
 
 import userRoutes from "./routes/UserRoute.js";
 import productRoutes from "./routes/ProductRoutes.js";
-import categoryRoutes from "./routes/CategoryRoutes.js";
 
 dotenv.config();
 
@@ -17,21 +14,10 @@ const app = express();
 app.use(cors());
 const PORT = 8080;
 app.use(bodyParser.json());
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     exposedHeaders: ["Content-Length", "X-Total-Count"],
-//     maxAge: 3600,
-//     optionsSuccessStatus: 200,
-//   })
-// );
 
 // Routes
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
-app.use("/category", categoryRoutes);
 
 // Database Connection
 mongoose
