84f8b131e501e45d8c6783ed1a8f0617ccd3a363
Thorik <axe.army194@gmail.com>
2024-10-31 19:14:29 +0700

    update hardcode

--- config/db.js
+++ config/db.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
-const dev_db_url =
-  "mongodb+srv://axearmy194:qUh0bMON24ewnN3T@myproject.ms3ju.mongodb.net/?retryWrites=true&w=majority&appName=MyProject";
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || dev_db_url);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
